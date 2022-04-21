17bf5d8c7207e2c22bec3b9e64ce4a4a62f436e4
Sri Sai Vidhya S <103099644+srisaividhya@users.noreply.github.com>
2022-04-21 18:00:06 +0530

    Update Product.java

--- Question 32/Product.java
+++ Question 32/Product.java
@@ -1,3 +1,57 @@
+/*Question 32:
+
+Your task here is to implement a Java code based on the following specifications. Note that your code should match the specifications in a precise manner. Consider default visibility of classes, data fields, and methods unless mentioned otherwise.
+Specifications:
+class Product
+  data members:
+     Integer id;
+     String name;
+     Double price;
+      visibility: private
+
+   Product(Integer id, String name, Double price): constructor with public visibility
+  Define getter setters with public visibility
+  toString() method has been implemented for you 
+
+class Implementation:
+ method definition:
+  getProductName(List<Product> products) :
+   return type: Map<String, List<Product>> 
+   visibility: public
+ 
+  getSum(List<Product> products) :
+    return type: Map<String, Double>
+    visibility: public
+Task:
+class Product:
+- define the class according to the above specifications
+class Implementation:
+Implement the below method for this class using in Stream API:
+•	Map<String, List<Product>> getProductName(List<Product> products): 
+fetch and return the details of all the products
+•	 Map<String, Double> getSum(List<Product> products):  sum all the product in the list and return it
+Sample Input
+Product pr1 = new Product(1, "Ceviche", 15.0);
+Product pr2 = new Product(2, "Chilaquiles", 25.50);
+Product pr3 = new Product(3, "Bandeja Paisa", 35.50);
+Product pr4 = new Product(4, "Ceviche", 15.0);
+
+List<Product> pr = Arrays.asList(pr1, pr2, pr3, pr4);
+Implementation imp = new Implementation();
+------------------------------------------------------
+
+imp.getProductName(pr)
+imp.getSum(pr)
+Sample Output
+{Bandeja Paisa=[Product{id=3, name='Bandeja Paisa', price=35.5}], Chilaquiles=[Product{id=2, name='Chilaquiles', price=25.5}]}
+---------------------------------------------------------
+{Ceviche=30.0, Bandeja Paisa=35.5, Chilaquiles=25.5}
+NOTE
+•	You can make suitable function calls and use the RUN CODE button to check your main() method output.
+ALLOWED TECHNOLOGIES
+•	Java 8*/
+
+
 package doselect32;
 import java.util.ArrayList;
 import java.util.Arrays;
