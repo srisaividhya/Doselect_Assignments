01d9c9bcd6de54874e5fee653f282755b39a558b
Sri Sai Vidhya S <103099644+srisaividhya@users.noreply.github.com>
2022-04-21 18:06:41 +0530

    Update Implementation.java

--- Question 60/Implementation.java
+++ Question 60/Implementation.java
@@ -1,3 +1,30 @@
+/*Question 60:
+
+Alan has recently learned about strings in his programming classes. He decided to create some interesting strings using the concepts.
+Help Alan!
+Implement a Java code based on the following specifications. Note that your code should match the specifications in a precise manner. Consider default visibility of classes, data fields and methods unless mentioned otherwise.
+Specifications:
+class Implementation:
+   method definiton:
+    splitAddAndReturnCharacter(String str)
+
+        visibility: public
+        return type: char
+Task:
+class Implementation
+-Implement the below method for this class
+•	char splitAddAndReturnCharacter(String str): Sum all the character values i.e. ‘a’ = 1, ‘b’ = 2, ‘c’ = 3, …, ‘z’ = 26 and return the character corresponding to the sum value. If it exceeds 26 then take sum % 26.
+Note: string str consisting of lowercase English alphabets only.
+Sample Input:
+java
+Sample Output
+h
+NOTE:
+•	You can make suitable function calls and use RUN CODE button to check your main() method output.
+ALLOWED TECHNOLOGIES
+•	Java 8 */
+
+
 package doselect60;
 
 import java.util.Scanner;
