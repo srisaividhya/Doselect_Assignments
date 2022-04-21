6c9348147738dcd9888ff64354d212d670ba3e05
Sri Sai Vidhya S <103099644+srisaividhya@users.noreply.github.com>
2022-04-21 17:56:44 +0530

    Update Dog.java

--- Question 31/Dog.java
+++ Question 31/Dog.java
@@ -1,3 +1,47 @@
+/*Question 31:
+
+Your task here is to implement a Java code based on the following specifications. Note that your code should match the specifications in a precise manner. Consider default visibility of classes, data fields, and methods unless mentioned otherwise.
+Specifications:
+class definitions: 
+  class Dog:
+    data members:
+     String name
+     int age
+     int weight
+
+   Dog(String name, int age, int weight): constructor with public visibility
+    Define getter setters with public visibility
+    toString(): has been implemented for you
+
+  class Implementation:
+     method definition:
+       filterByAgeAndWeight(List<Dog> listDog):
+           return type: List<Dog> 
+           visibility: public
+
+       separateWithDelimeter(List<Dog> listDog):
+           return type: String 
+           visibility: public
+Task:
+class Dog:
+- Define the class according to the above specifications
+class Implementation:
+Implement the below method for this class:
+•	List<Dog> filterByAgeAndWeight(List<Dog> listDog): fetch dog details on the basis of:
+•	age greater than 10
+•	weight greater than 25
+•	get the filtered data, put it into a list and return the list
+•	String separateWithDelimeter(List<Dog> listDog):  concat and return the dogs details with delimeter "$~$~" 
+Refer sample output for clarity
+Sample Input
+ List<Dog> list = new ArrayList<Dog>();
+  list.add(new Dog("German Shepherd ", 20, 35));
+  list.add(new Dog("Labrador ", 5, 40));
+  list.add(new Dog("Pitbull ", 29, 100));
+  list.add(new Dog("Poodle", 10, 45));
+Sample Output
+[Dog{name='German Shepherd ', age=20, weight=35}, Dog{name='Pitbull ', age=29, weight=100}] */
+
 package doselect31;
 
 import java.util.ArrayList;
