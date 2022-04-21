12b6a038869da83057f379d5a66f119512675ae3
Sri Sai Vidhya S <103099644+srisaividhya@users.noreply.github.com>
2022-04-21 18:02:37 +0530

    Update Bomb.java

--- Question 34/Bomb.java
+++ Question 34/Bomb.java
@@ -1,3 +1,68 @@
+/*Question 34:
+
+Complete the classes using the Specifications given below. Consider default visibility of classes, data fields, and methods unless mentioned otherwise.
+Specifications
+class definitions:
+ class Bomb:
+    data members: 
+        int time
+        String color
+    Bomb(int time, String color): constructor with public visibility
+
+class Suicide:
+ data members:
+     Bomb bomb
+
+     Suicide(Bomb bb): constructor with public visibility
+
+ method definitions:
+     diffuseIt(int time, String color) throws Exception:     
+        return type: String
+        visibility: public     
+
+     checkSafety(int time, String color) throws Exception:
+        return type: String
+        visibility: public   
+
+ class SuicideException extends Exception:
+   method definition:
+      SuicideException(String msg)
+         visibility: public  
+Task
+Class Bomb
+- define the int variable time.
+-define String variable color
+-define a constructor according to the above specifications.
+Class Suicide
+-define the bomb variable with null;
+-define a constructor according to the above specifications and initialize the bomb variable with the object passed in the argument.
+Implement the below methods for this class:
+-String diffuseIt(int time, String color) throws Exception:
+•	Write a code to validate the criteria for getting the award.
+•	throw a SuicideException if time is greater than the time of the bomb with the message "Time exceeded".
+•	throw a SuicideException if the color of the bomb is different from the color passed as an argument with the message "Wrong color".
+•	If no above exception is found then return a string message "Hope is there".
+-String checkSafety(int time, String color) throws Exception:
+•	Write a code to send an invite to the nominee.
+•	If diffuseIt() method throws a SuicideException then returns a message "Bomb exploded".(Use try-catch block)
+•	If it throws any other exception then return a message "Other exception".
+•	If no exception is found then return a message "Take a bow".
+Sample Input
+Bomb b=new Bomb(10,"red");
+Suicide sc=new Suicide(b);
+String s = sc.diffuseIt(5,"red");
+String t = sc.checkSafety(8,"blue");
+s.toLowerCase();
+t.toLowerCase();
+Sample Output
+hope is there
+bomb exploded
+NOTE:
+•	You can make suitable function calls and use the RUN CODE button to check your main() method output.
+ALLOWED TECHNOLOGIES
+•	Java 8
+*/
+
 
 package doselect34;
 
